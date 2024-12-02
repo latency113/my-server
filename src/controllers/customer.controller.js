1eceec332e34fdaa52db6b218f9a26d5375d22cb
Khan <49835781+latency113@users.noreply.github.com>
2024-12-02 19:57:40 +0700

    Update customer.controller.js

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -5,13 +5,13 @@ exports.getName = (req,res)=>{
     res.send("Customer Name:" + req.params.name);
 }
 exports.editName = (req,res)=>{
-    res.send("Customer Name:");
+    res.send("Edit Customer Name:" + req.params.id);
 }
 exports.create = (req,res)=>{
     res.send("New customer");
 }
 exports.getById = (req,res)=>{
-    res.send("Edit Customers ID:" + req.params.id);
+    res.send("Customers ID:" + req.params.id);
 }
 exports.patch = (req,res)=>{
     res.send("Patch customers ID:" + req.params.id);
